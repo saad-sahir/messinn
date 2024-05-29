beee87a1b94bd6d813d29c461e81be2736934477
Saad Sahir <133065206+saad-sahir@users.noreply.github.com>
2024-05-29 18:02:08 +0200

    Delete mask.py

--- mask.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-def mask(image):
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    mask_green = cv2.inRange(hsv, (66, 75, 75), (86, 255, 255))
-    masked_image = cv2.bitwise_and(image, image, mask=mask_green)
-    return masked_image
